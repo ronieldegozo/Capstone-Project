ae53227a55d808a6905fe11fc328683f529a11d0
Mc Roniel De Gozo <ronieldegozo@gmail.com>
2021-06-30 14:56:26 +0800

    adding a security for the registered student

--- controller/emailController.php
+++ controller/emailController.php
@@ -12,7 +12,6 @@ $transport = (new Swift_SmtpTransport('smtp.gmail.com', 465, 'ssl'))
 // Create the Mailer using your created Transport
 $mailer = new Swift_Mailer($transport);
 
-
 // Send the message
 function sendVerificationEmail($userEmail, $token){
     // Create a message
@@ -25,29 +24,30 @@ function sendVerificationEmail($userEmail, $token){
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Verify Email</title>
+        <style>
+        .btn{
+            font: bold 11px Arial;
+            text-decoration: none;
+            background-color: #6cc7b0;
+            color: #ffffff;
+            padding: 2px 6px 2px 6px;
+ 
+                }
+            </style>
     </head>
     <body>
-        <div class="wrapper">
-            <p>Thank you for signing in our Website, Please click the link below to verify your email</p>
-        </div>
-        <a href="http://localhost/capstone%20project/homepage.php?token=' . $token .'">Verify your email</a>
+    <div class="wrapper">
+        <p>Thank you for signing in New Era University, Please click the link below to Verify your Email</p>
+    </div>
+        <a href="http://localhost/capstone%20project/homepage.php?token=' . $token .'" class="btn">Verify your email</a>
     </body>
     </html>
     ';
-
-
-    $message = (new Swift_Message('Verify your Email Address'))
+    $message = (new Swift_Message('New Era University, Verify your Email Address'))
     ->setFrom('lauritosamber@gmail.com')
     ->setTo($userEmail)
     ->setBody($body,'text/html');
-
     $result = $mailer->send($message);
-    
-
 }
-
-// function sendPassword(Type $var = null){
-
-// }
 
 ?>
--- student/includes/header.php
+++ student/includes/header.php
@@ -5,8 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Student Dashboard</title>
-    <link rel="stylesheet" href="../public/studentdashboard/css/style.css">
-    <link rel="stylesheet" href="../public/studentdashboard/css/sb-admin-2.min.css">
-    <link rel="stylesheet" href="../public/studentdashboard/css/all.min.css">
-    <link rel="shortcut icon" href="../public/assets/img/neu.png" />
+    <link rel="stylesheet" href="./public/studentdashboard/css/style.css">
+    <link rel="stylesheet" href="./public/studentdashboard/css/sb-admin-2.min.css">
+    <link rel="stylesheet" href="./public/studentdashboard/css/all.min.css">
+    <link rel="shortcut icon" href="./public/assets/img/neu.png" />
 </head>
